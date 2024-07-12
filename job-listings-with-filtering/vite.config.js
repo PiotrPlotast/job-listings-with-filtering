7b9b937db524cf20e7fa9a8629332183f96524d6
Pietrell <piotrplotast2006@gmail.com>
2024-07-12 15:49:35 +0200

    modify build

--- job-listings-with-filtering/vite.config.js
+++ job-listings-with-filtering/vite.config.js
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/job-listings-with-filtering/",
+  base: "",
   plugins: [react()],
 });
